49203ca7e0b496852d6a42f14a8e9b98fcaeae26
h-alali <121454711+h-alali@users.noreply.github.com>
2024-03-18 01:06:28 -0400

    Update task 1.py

--- Algo and DS Final Project/task 1.py
+++ Algo and DS Final Project/task 1.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
+
 # Create an empty graph
 G = nx.Graph()
 
@@ -8,18 +10,14 @@ def nodes():
     with open('nodes.txt','r') as f:
         data = f.read().split(',')
     return data
-    
-
 
 # Add nodes to the graph
 G.add_nodes_from(nodes())
 
-
 def edges():
     with open('edges.txt','r') as f:
         lines = f.readlines()
-    return [tuple(line.strip().split(',')) for line in lines]
-
+    return [(node1.strip(), node2.strip(), int(weight)) for node1, node2, weight in (line.split(',') for line in lines if len(line.split(',')) == 3)]
 
 # Add weighted edges to the graph
 G.add_weighted_edges_from(edges())
@@ -43,7 +41,7 @@ nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.5,
 def FindShortestPath():
 
     #asks user to input the node they want to start at
-    startNode = input("What node would you like to start on?: ")
+    startNode = input("What node would you like to start on?: ").upper()
 
     #A list of the destinations
     chargeStationList = ["H", "K", "Q", "T"]
@@ -57,3 +55,5 @@ FindShortestPath()
 
 # Show the plot
 plt.show()
+print("Nodes:", nodes())
+print("Edges:", edges())
